d43be4c5451f8e83c5618d320646cefabe8feda8
lx <1120722567@qq.com>
2019-11-12 17:44:33 +0800

    modify version info

--- footmark/__init__.py
+++ footmark/__init__.py
@@ -5,7 +5,7 @@ import os
 
 from footmark.pyami.config import Config, FootmarkLoggingConfig, DefaultLoggingConfig
 
-__version__ = '1.13.0'
+__version__ = '1.14.0'
 Version = __version__  # for backware compatibility
 
 
